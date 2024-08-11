ca4551702d0b0d0227ccb1ab81f59e2bdc70d6ac
sarim2000 <sarimbleedblue@gmail.com>
2024-08-11 21:36:51 +0530

    update

--- src/auth.ts
+++ src/auth.ts
@@ -1,40 +1,54 @@
-import { ConvexAdapter } from '@/app/ConvexAdapter';
-import { SignJWT, importPKCS8 } from 'jose';
-import NextAuth from 'next-auth';
-import Google from 'next-auth/providers/google';
+import { ConvexAdapter } from "@/app/ConvexAdapter";
+import { SignJWT, importPKCS8 } from "jose";
+import NextAuth from "next-auth";
+import Google from "next-auth/providers/google";
 
-const CONVEX_SITE_URL = process.env.NEXT_PUBLIC_CONVEX_URL!.replace(/.cloud$/, '.site');
+if (process.env.CONVEX_AUTH_PRIVATE_KEY === undefined) {
+  throw new Error(
+    "Missing CONVEX_AUTH_PRIVATE_KEY Next.js environment variable",
+  );
+}
+
+if (process.env.NEXT_PUBLIC_CONVEX_URL === undefined) {
+  throw new Error(
+    "Missing NEXT_PUBLIC_CONVEX_URL Next.js environment variable",
+  );
+}
+
+const CONVEX_SITE_URL = process.env.NEXT_PUBLIC_CONVEX_URL!.replace(
+  /.cloud$/,
+  ".site",
+);
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  debug: true,
-  providers: [Google],
+  providers: [
+    Google
+  ],
   adapter: ConvexAdapter,
-  trustHost: true,
   callbacks: {
+    // Attach a JWT for authenticating with Convex
     async session({ session }) {
-      const privateKeyString = process.env.CONVEX_AUTH_PRIVATE_KEY!;
-      // Ensure the key is in PEM format
-      const pemKey = privateKeyString.startsWith('-----BEGIN PRIVATE KEY-----')
-        ? privateKeyString
-        : `-----BEGIN PRIVATE KEY-----\n${privateKeyString}\n-----END PRIVATE KEY-----`;
-
-      const privateKey = await importPKCS8(pemKey, 'RS256');
-
+      const privateKey = await importPKCS8(
+        process.env.CONVEX_AUTH_PRIVATE_KEY!,
+        "RS256",
+      );
       const convexToken = await new SignJWT({
+        // These fields will be available on `ctx.auth.getUserIdentity()`
+        // in Convex functions:
         sub: session.userId,
       })
-        .setProtectedHeader({ alg: 'RS256' })
+        .setProtectedHeader({ alg: "RS256" })
         .setIssuedAt()
         .setIssuer(CONVEX_SITE_URL)
-        .setAudience('convex')
-        .setExpirationTime('1h')
+        .setAudience("convex")
+        .setExpirationTime("1h")
         .sign(privateKey);
-      return { ...session, convexToken, userId: session.userId };
+      return { ...session, convexToken };
     },
   },
 });
 
-declare module 'next-auth' {
+declare module "next-auth" {
   interface Session {
     convexToken: string;
   }
